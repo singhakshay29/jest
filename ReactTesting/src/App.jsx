adf3921c5e07867e011fe482d592b3785aa82b2c
akshaysingh29 <akshay2898.as@gmail.com>
2026-03-31 12:50:55 +0530

    testing components

--- ReactTesting/src/App.jsx
+++ ReactTesting/src/App.jsx
@@ -1,10 +1,14 @@
+import {useState} from "react";
 import "./App.css";
 
 function App() {
+  const [name,setName]=useState('');
   return (
     <>
       <div>
         <h2>Hello Akshay</h2>
+        <input type="text" value={name} onChange={(e)=>setName(e.target.value)}/>
+        <button onClick={() => console.log("I m Clicked")}>Click Me!</button>
       </div>
     </>
   );
--- ReactTesting/src/App.test.jsx
+++ ReactTesting/src/App.test.jsx
@@ -1,10 +1,19 @@
-import {render} from "@testing-library/react"
-import App from "./App"
-import { test } from "vitest";
-test("app should display name",()=>{
-    const {getByText,debug} =render(<App/>);
-    const mainContent  =getByText('Hello Akshay')
-    console.log(mainContent);
-    
-    debug();
-})
+import { fireEvent, render } from "@testing-library/react";
+import App from "./App";
+import { test, expect } from "vitest";
+
+test("app should display name", () => {
+  const { getByText,getByRole } = render(<App />);
+  const mainContent = getByText("Hello Akshay");
+
+  expect(mainContent).toBeInTheDocument();
+  let btn=getByRole('button');
+  fireEvent.click(btn);
+  
+  let input=getByRole('textbox');
+  expect(input).toHaveValue('');
+  
+  fireEvent.change(input,{target:{value:'Akshay'}});
+  expect(input).toHaveValue('Akshay');
+  
+});
