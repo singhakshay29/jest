14611c04156aded6fe07ea80e2aafc877139d790
akshaysingh29 <akshay2898.as@gmail.com>
2026-04-05 12:03:28 +0530

    updates jest testing cases

--- first.test.js
+++ first.test.js
@@ -1,3 +1,5 @@
+const obj = require("./index");
+
 let user=[];
 beforeAll(()=>{
     console.log('befor all');
@@ -23,6 +25,34 @@ test.skip("second",()=>{
     console.log('second');
 })
 
-test.only("third",()=>{
+test("third",()=>{
     console.log('third');
 })
+
+//example of mock function
+
+const square= jest.fn();
+describe('Should test Square file',()=>{
+    
+    //method One by importing
+    //test('Test X function',()=>{
+    //    const sq= x(3);
+    //    expect(sq).toBe(9);
+    //})
+    
+    
+    //Second Method
+      test('Test Square function using Mock',()=>{
+       square.mockImplementation((value)=>value*value);
+       const result=square(5);
+       expect(result).toBe(25);
+    })
+    test('Test X function using Spy',()=>{
+        const spy = jest.spyOn(obj, "x");
+       spy.mockImplementation((value)=>value*value);
+        const result=obj.x(5);;
+        expect(result).toBe(25);
+        spy.mockRestore();
+     })
+})
+
--- index.js
+++ index.js
@@ -16,13 +16,32 @@ function fetch(callback) {
 }
 
 function fetchPromise() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     setTimeout(() => resolve("butter"), 1000);
   });
+}
+function x(value){
+  return value*value;
 }
 
 //mock functions
 
 // const mockCallback = jest.fn((x) => 42 + x);
 
-module.exports = { sum, fn, fetch, fetchPromise };
+module.exports = { sum, fn, fetch, fetchPromise,x };
+
+//jest.fn() (Mock function)
+//• Creates a standalone fake function. 
+//• Not linked to any real module or object. 
+//• You decide its behaviour using mockReturn Value, mockImplementation, etc. 
+//• Purely for testing when you don't have (or don't want to use) a real function.
+
+//jest.spyOn(obj, methodName) (Spy) 
+
+//• A spy is basically a wrapper around an existing function 
+//• Attaches to an existing function inside an object or module. 
+//• Let's you track calls: how many times it was called, with what arguments. 
+//• You can override its behavior temporarily using mockReturn Vali or mockImplementation
+
+//Mock (jest.fn) → brand new fake function. 
+//Spy (iest.spyOn) - monitor or temporarily change a real
